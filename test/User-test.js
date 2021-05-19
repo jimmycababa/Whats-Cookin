b05bcc8fd4709bd18202c89014b86e4c320573d5
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-18 18:19:08 -0600

    Preliminary lint of User-test

--- test/User-test.js
+++ test/User-test.js
@@ -1,26 +1,27 @@
 import { expect } from 'chai';
 import { User } from "../src/User";
 import { Ingredient } from '../src/classes/Ingredient';
+import { Recipe } from '../src/classes/Recipe';
 
 describe("User", () => {
   let user, ingredient, recipe;
   beforeEach(() => {
     ingredient = new Ingredient(1124, "jelly beans", 625);
     user = new User(55, "Sally HungryPerson", [{"ingredient": 1124, "amount": 3}, {"ingredient": 1127, "amount": 2}, {"ingredient": 14, "amount": 7}], ingredient);
-    });
-    recipe = new Recipe(51, "https://www.pumpkinnspice.com/wp-content/uploads/2020/08/creamy-macaroni-cheese-4.jpg", [{"id": 20, "quantity": {"amount": 2, "unit": "c"}}, {"id": 8, "quantity": {"amount": 9, "unit": "oz"}}, {"id": 11, "quantity": { "amount": 1.5, "unit": "cup"}}, {"id": 225, "quantity": {"amount": 0.5, "unit": "lb"}],
+    //});
+    recipe = new Recipe(51, "https://www.pumpkinnspice.com/wp-content/uploads/2020/08/creamy-macaroni-cheese-4.jpg", [{"id": 20, "quantity": {"amount": 2, "unit": "c"}}, {"id": 8, "quantity": {"amount": 9, "unit": "oz"}}, {"id": 11, "quantity": { "amount": 1.5, "unit": "cup"}}, {"id": 225, "quantity": {"amount": 0.5, "unit": "lb"}}],
       [{ "instruction": "Salt the water then boil", "number": 1},
       {"instruction": "Pour the dry noodles into the pot", "number": 2},
       {"instruction": "Boil noodles for 8 minutes", "number": 3},
       {"instruction": "Make a roux with flour and butter", "number": 4},
       {"instruction": "Add cheese to roux", "number": 5},
       {"instruction": "Add cheesy roux to noodles and mix", "number": 6}],
-      "Macaroni and Cheese", ["comfort food", "dinner"];
+      "Macaroni and Cheese", ["comfort food", "dinner"]);
     });
 
     it('should be a function', () => {
 
-      expect(User).to.be.a.function());
+      expect(User).to.be.a.function();
     });
 
   it('should be an instance of User', () => {
@@ -36,7 +37,7 @@ describe("User", () => {
 
   it('should store the name of the user', () => {
 
-    expect(user.name).to.be.string());
+    expect(user.name).to.be.string();
     expect(user.name).to.equal("Sally HungryPerson");
   });
 
