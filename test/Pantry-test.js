c7662c049c49c1d3c6d6b59b8a3356eb29e4bd13
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-18 19:01:52 -0600

    Add setup for Pantry test

---
+++ test/Pantry-test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import { User } from "../src/classes/User";
+import { Ingredient } from '../src/classes/Ingredient';
+import { Recipe } from '../src/classes/Recipe';
+
+describe("Pantry", () => {
+  let user, recipe, pantry;
+
+
+
+});
