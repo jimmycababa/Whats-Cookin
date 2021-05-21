8308663d7ecd794b8dc2f8e7971216dab185d065
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-21 14:14:02 -0600

    Add notes to scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,9 @@
 import './styles.css';
 import apiCalls from './apiCalls';
 
+//We will be instantiating Recipes here from data
+//recipeRepo instantiation here  
+// kicking off the whole project
 
 
 
