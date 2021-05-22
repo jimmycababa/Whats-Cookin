e55fa20e95cb59ae8eb66e3f346de17634658812
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-05-21 21:19:02 -0600

    Add display all recipes function

--- src/scripts.js
+++ src/scripts.js
@@ -17,6 +17,7 @@ const toCookButton = document.getElementById("toCookButton");
 const addToFavoriteButton = document.getElementById("addToFavoriteButton");
 const addtoCookButton = document.getElementById("addtoCookButton");
 // Submit Buttons
+const filterNameIngInput = document.getElementById("filterNameIngInput");
 const submitNameIng = document.getElementById("submitNameIng");
 const submitTagsButton = document.getElementById("submitTagsButton");
 //Tags
@@ -45,14 +46,15 @@ const recipeDisplay = document.getElementById("recipeDisplay");
 const favRecipesView = document.getElementById("favRecipesView");
 const toCookRecipesView = document.getElementById("toCookRecipesView");
 const currentRecipeView = document.getElementById("currentRecipeView");
-
+const currentRecipeCard = document.getElementById("currentRecipeCard");
 
 // Event Listeners
+allRecipesButton.addEventListener('click', showAllRecipes);
+submitNameIng.addEventListener('click', searchByNameIng);
+submitTagsButton.addEventListener('click', searchByTags);
 // favoriteButton.addEventListener('click', showFavoriteRecipes);
-// toCookButton.addEventListener('click', showRecipesToCook);
-// submitNameIng.addEventListener('click', searchByNameIng);
-// submitTagsButton.addEventListener('click', searchByTags);
 // addToFavoriteButton.addEventListener('click', );
+// toCookButton.addEventListener('click', showRecipesToCook);
 // addtoCookButton.addEventListener('click', );
 
 
@@ -65,35 +67,121 @@ function hide(element) {
   element.classList.add('hidden');
 }
 
-function showFavoriteRecipes() {
-  show(favRecipesView);
-  hide(toCookRecipesView);
+// Show Recipes Function
+// Change for iteration methods and be able to use for fav and to cook
+function showRecipes(recipes) {
+  // console.log(recipes);
+  for (var i = 0; i < recipes.length; i++) {
+    recipes[i];
+
+    // console.log(recipes[i]);
+    let recipeCard = document.createElement("div");
+    recipeCard.innerHTML = `<p>${recipes[i].name}</p>
+    <img src=${recipes[i].image}>`
+    recipeDisplay.appendChild(recipeCard)
+  }
 }
 
-function showRecipesToCook() {
-  show(toCookRecipesView);
+function showAllRecipes() {
+  show(recipeDisplay);
+  hide(toCookRecipesView);
   hide(favRecipesView);
+  hide(currentRecipeView);
+
+  showRecipes(recipeRepo.recipes);
 }
 
 function searchByNameIng() {
-
+  show(recipeDisplay);
+  hide(toCookRecipesView);
+  hide(favRecipesView);
+  hide(currentRecipeView);
+  // filterNameIngInput.value"
+  // Will take the value from the user tags selection
+  //That value will pass as an "Argument" through:
+  // recipeRepository.findIngredientNames(filterNameIngInput.value)
+  // Is gonna iterate and filter all the recipes repository info and will return a new array based the "User conditions".
+  // That new array will be the ARGUMENT for  "showRecipes();"
+  showRecipes();
 }
+
 
 function searchByTags() {
-
+  show(recipeDisplay);
+  hide(toCookRecipesView);
+  hide(favRecipesView);
+  hide(currentRecipeView);
+  // " checkboxes value"
+  // this query will take the value from the user selections
+  //That value will pass an "Argument" through:
+  // recipeRepository.""???findIngredientNames(""filterNameIngInput.value)
+  // Is gonna iterate and filter all the recipes repository info and will return a new array based the "User conditions".
+  // That new array will be the ARGUMENT for  "showRecipes();"
+  showRecipes();
 }
 
-// function showRecipes(recipes) {
-//   console.log(recipes);
-//   for (var i = 0; i < recipes.length; i++) {
-//     recipes[i];
-//     console.log(recipes[i]);
-//     let recipeCard = document.createElement("div");
-//     recipeCard.innerHTML = `<p>${recipes[i].name}</p>
-//     <img src=${recipes[i].image}>`
-//     recipeDisplay.appendChild(recipeCard)
-//   }
+function displayCurrentRecipe(currentRecipe) {
+      let currentRecipeHTML +=
+          `<div class="current-recipe-card" id="currentRecipeCard">
+          <section class="current-recipe-name">
+            <h3>${currentRecipe.name}</h3>
+          </section>
+          <section class="current-recipe-add-to-fav">
+            <button class="add-favorite-button" id="addToFavoriteButton">Favorite</button>
+            <div class="current-recipe-add-to-cook">
+              <button class="add-to-cook-button" id="addtoCookButton">Add to Weekly Meals</button>
+            </div>
+          </section>
+          <section class="current-recipe-img">
+            <div class="current-recipe-ing">
+              <ul>
+                <li>${currentRecipe.findIngredientNames()}</li>
+              </ul>
+            </div>
+            <div class="food-image">
+              <img src=${currentRecipe.image}>
+            </div>
+          </section>
+          <section class="current-recipe-text">
+            <div class="current-recipe-inst">
+              <ol>
+                <li>${currentRecipe.findIngredientNames}</li>
+              </ol>
+            </div>
+            <div class="current-recipe-tags">
+              <ul>
+                <li>${recipes.tags}</li>
+              </ul>
+            </div>
+          </section>
+        </div>`
+
+    // currentRecipeView.innerHTML = currentRecipeHTML;
+    currentRecipeView.appendChild(currentRecipeHTML)
+};
+
+function showCurrentRecipe() {
+  show(currentRecipeView);
+  hide(recipeDisplay);
+  hide(toCookRecipesView);
+  hide(favRecipesView);
+
+  displayCurrentRecipe();
+};
+
+// function showFavoriteRecipes() {
+//   show(favRecipesView);
+//   hide(toCookRecipesView);
 // }
+//
+// function showRecipesToCook() {
+//   show(toCookRecipesView);
+//   hide(favRecipesView);
+// }
+
+
+
+
 // window.onload = showRecipes(recipeRepo.recipes);
 
 
