d93425163f4a00cd73284af68d982ce3d6e6ff51
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-23 10:05:08 -0600

    Add partial filter functionality

--- src/scripts.js
+++ src/scripts.js
@@ -30,25 +30,25 @@ const filterNameIngInput = document.getElementById("filterNameIngInput");
 const submitNameIng = document.getElementById("submitNameIng");
 const submitTagsButton = document.getElementById("submitTagsButton");
 //Tags
-const antipastiTag = document.getElementById("antipastiTag");
-const starterTag = document.getElementById("starterTag");
-const snackTag = document.getElementById("snackTag");
-const appetizerTag = document.getElementById("appetizerTag");
-const antipastoTag = document.getElementById("antipastoTag");
-const horDoeuvreTag = document.getElementById("horDoeuvreTag");
-const lunchTag = document.getElementById("lunchTag");
-const mainCourseTag = document.getElementById("mainCourseTag");
-const mainDishTag = document.getElementById("mainDishTag");
-const dinnerTag = document.getElementById("dinnerTag");
-const sauceTag = document.getElementById("sauceTag");
-const sideDishTag = document.getElementById("sideDishTag");
-const brunchTag = document.getElementById("brunchTag");
-const morningMealTag = document.getElementById("morningMealTag");
-const dipTag = document.getElementById("dipTag");
-const breakfastTag = document.getElementById("breakfastTag");
-const spreadTag = document.getElementById("spreadTag");
-const saladTag = document.getElementById("saladTag");
-const condimentTag = document.getElementById("condimentTag");
+// const antipastiTag = document.getElementById("antipastiTag");
+// const starterTag = document.getElementById("starterTag");
+// const snackTag = document.getElementById("snackTag");
+// const appetizerTag = document.getElementById("appetizerTag");
+// const antipastoTag = document.getElementById("antipastoTag");
+// const horDoeuvreTag = document.getElementById("horDoeuvreTag");
+// const lunchTag = document.getElementById("lunchTag");
+// const mainCourseTag = document.getElementById("mainCourseTag");
+// const mainDishTag = document.getElementById("mainDishTag");
+// const dinnerTag = document.getElementById("dinnerTag");
+// const sauceTag = document.getElementById("sauceTag");
+// const sideDishTag = document.getElementById("sideDishTag");
+// const brunchTag = document.getElementById("brunchTag");
+// const morningMealTag = document.getElementById("morningMealTag");
+// const dipTag = document.getElementById("dipTag");
+// const breakfastTag = document.getElementById("breakfastTag");
+// const spreadTag = document.getElementById("spreadTag");
+// const saladTag = document.getElementById("saladTag");
+// const condimentTag = document.getElementById("condimentTag");
 
 const checkBoxes = document.querySelectorAll("input[type=checkbox]");
 // Views
@@ -63,9 +63,11 @@ const currentRecipeCard = document.getElementById("currentRecipeCard");
 allRecipesButton.addEventListener('click', showAllRecipes);
 submitNameIng.addEventListener('click', searchByNameIng);
 submitTagsButton.addEventListener('click', searchByTags);
+recipeDisplay.addEventListener("click", showCurrentRecipe);
 
 window.addEventListener("load", function() {
   instantiateRecipes(recipeData)});
+
 // favoriteButton.addEventListener('click', showFavoriteRecipes);
 // addToFavoriteButton.addEventListener('click', );
 // toCookButton.addEventListener('click', showRecipesToCook);
@@ -87,17 +89,29 @@ function hide(element) {
 
 // Show Recipes Function
 // Change for iteration methods and be able to use for fav and to cook
+
 function showRecipes(recipes) {
   // console.log(recipes);
+  // recipeDisplay.innerHTML = "";
+  // recipes.forEach(recipe => {
+  //   recipeDisplay.innerHTML +=
+  //
+  //   `
+  //   <p id=${recipe.id}>${recipe.name}</p>
+  //   <img id=${recipe.id} src=${recipe.image}>
+  //   `
+  //
+  // })
+
   for (var i = 0; i < recipes.length; i++) {
-    recipes[i];
+    //recipes[i];
 
     // console.log(recipes[i]);
     let recipeCard = document.createElement("div");
     recipeCard.innerHTML =
     `
-    <p>${recipes[i].name}</p>
-    <img src=${recipes[i].image}>
+    <p id=${recipes[i].id}>${recipes[i].name}</p>
+    <img id=${recipes[i].id} src=${recipes[i].image}>
     `
     recipeDisplay.appendChild(recipeCard)
   }
@@ -119,15 +133,15 @@ function searchByNameIng() {
   hide(currentRecipeView);
   preventDefault();
   show(recipeDisplay);
-  // filterNameIngInput.value"
-  // Will take the value from the user tags selection
-  //That value will pass as an "Argument" through:
-  // recipeRepository.findIngredientNames(filterNameIngInput.value)
-  // Is gonna iterate and filter all the recipes repository info and will return a new array based the "User conditions".
-  // That new array will be the ARGUMENT for  "showRecipes();"
   showRecipes();
 }
 
+// filterNameIngInput.value"
+// Will take the value from the user tags selection
+//That value will pass as an "Argument" through:
+// recipeRepository.findIngredientNames(filterNameIngInput.value)
+// Is gonna iterate and filter all the recipes repository info and will return a new array based the "User conditions".
+// That new array will be the ARGUMENT for  "showRecipes();"
 
 function searchByTags() {
   hide(toCookRecipesView);
@@ -136,28 +150,17 @@ function searchByTags() {
   preventDefault();
   show(recipeDisplay);
 
-  //console.log("search by tags here")
-
   let checkBoxMatches = [];
   checkBoxes.forEach(checkBox => {
-    //console.log(checkBox)
     if(checkBox.checked) {
-
       checkBoxMatches.push(checkBox.value)
     }
   })
 
-  //console.log(checkBoxMatches)
   const tagMatches = recipeRepo.filterRecipesTags(checkBoxMatches);
-  //console.log(tagMatches)
+
   showRecipes(tagMatches)
-  // const checkBoxMatches = checkBoxes.filter(checkBox => {
-  //   console.log(checkBox.checked)
-  //   return checkBox.checked
-  }
-
-
-
+}
 
   // " checkboxes value"
   // this query will take the value from the user selections
@@ -168,7 +171,14 @@ function searchByTags() {
   //showRecipes();
 
 
+
+
+
+
 function displayCurrentRecipe(currentRecipe) {
+    currentRecipeView.innerHTML = "";
+    //console.log("displayCurrentRecipe ")
+    console.log(currentRecipe)
       currentRecipeView.innerHTML +=
           `<div class="current-recipe-card" id="currentRecipeCard">
           <section class="current-recipe-name">
@@ -183,7 +193,7 @@ function displayCurrentRecipe(currentRecipe) {
           <section class="current-recipe-img">
             <div class="current-recipe-ing">
               <ul>
-                <li>${currentRecipe.findIngredientNames()}</li>
+                <li>${currentRecipe.findIngredientNames(ingredientsData)}</li>
               </ul>
             </div>
             <div class="food-image">
@@ -193,29 +203,57 @@ function displayCurrentRecipe(currentRecipe) {
           <section class="current-recipe-text">
             <div class="current-recipe-inst">
               <ol>
-                <li>${currentRecipe.findIngredientNames}</li>
+                <li>${currentRecipe.findIngredientNames(ingredientsData)}</li>
               </ol>
             </div>
             <div class="current-recipe-tags">
               <ul>
-                <li>${recipes.tags}</li>
+                <li>${currentRecipe.tags}</li>
               </ul>
             </div>
           </section>
         </div>`
 
     // currentRecipeView.innerHTML = currentRecipeHTML;
-    currentRecipeView.appendChild(currentRecipeHTML)
+    //currentRecipeView.appendChild(currentRecipeView)
 };
+
+function instantiateRecipes(recipeData) {
+  recipeData.map(recipe => {
+    recipe = new Recipe(recipe.id, recipe.image, recipe.ingredients, recipe.instructions, recipe.name, recipe.tags)
+    recipe.createFullIngredients(ingredientsData)
+    //console.log(recipe)
+    return instantiatedRecipes.push(recipe)
+  })
+}
 
 function showCurrentRecipe() {
   show(currentRecipeView);
   hide(recipeDisplay);
   hide(toCookRecipesView);
   hide(favRecipesView);
+  let match;
+  recipeRepo.recipes.find(recipe => {
+    //console.log(event.target)
+    //event target closest for class
+    //event target.id also? 
+    match = event.target.closest(`${recipe.name}`)
 
-  displayCurrentRecipe();
+    return match
+  }
+  })
+    // displayCurrentRecipe(recipe)
+  // recipeRepo.recipes.find(recipe => {
+  //   console.log(event.target.id)
+  //   // console.log(recipe.id)
+  //   // if (event.target.id === parseInt(recipe.id)) {
+  //   //   match = recipe;
+  //   //   console.log(match)
+  //   // }
+  // }
 };
+
+
 
 // function showFavoriteRecipes() {
 //   show(favRecipesView);
@@ -231,14 +269,6 @@ function showCurrentRecipe() {
 
 
 // window.onload = showRecipes(recipeRepo.recipes);
-function instantiateRecipes(recipeData) {
-  recipeData.map(recipe => {
-    recipe = new Recipe(recipe.id, recipe.image, recipe.ingredients, recipe.instructions, recipe.name, recipe.tags)
-    recipe.createFullIngredients(ingredientsData)
-    //console.log(recipe)
-    return instantiatedRecipes.push(recipe)
-  })
-}
 //recipe.createFullIngredients()
 
 console.log('Hello world');
