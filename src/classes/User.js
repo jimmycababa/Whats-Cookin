274f2751d309f97bf05f51a5d164f22e061d4ad1
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-23 18:58:54 -0600

    Add properties to User class

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,10 +1,24 @@
 //     import { expect } from 'chai';
 // import  Recipe  from "../src/classes/Recipe";
-import  testIngredientsData  from './test-data';
+//import  testIngredientsData  from './test-data';
 
 class User {
   constructor(id, name, pantry) {
+    this.id = id;
+    this.name = name;
+    this.pantry = pantry;
+    this.favoriteRecipes = [];
+    this.mealsForTheWeek = [];
+    }
 
+  // Allow a user to favorite or unfavorite recipes (add to / remove from the user’s favoriteRecipes)
+
+
+
+  // Filter my favoriteRecipes by one or more tags.
+  // Filter my favoriteRecipes by its name or ingredients.
+
+    //add a test to remove the recipe from favorites
 }
 
 
