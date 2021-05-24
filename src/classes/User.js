a2fab85a67ce019b573fa27fae66ccc4a059d1e0
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-05-23 19:37:07 -0600

    Complete filterFavRecipesByName method on User class

--- src/classes/User.js
+++ src/classes/User.js
@@ -44,12 +44,40 @@ class User {
     if (!matches[0]) {
       return "Sorry, we could not find any recipes with that tag"
     } else {
-
       return matches
     }
   }
 
   // Filter my favoriteRecipes by its name or ingredients.
+  filterFavRecipesByName(input) {
+    const recipeMatch = this.favoriteRecipes.filter(recipe => {
+        return recipe.name  === input;
+    });
+    // if (!recipeMatch[0]) {
+    //   return this.filterFavRecipesByIngredients(input)
+    // } else {
+      return recipeMatch
+    }
+  //}
+
+  // filterFavRecipesByIngredients(input) {
+  //   let matches = [];
+  //   let match = this.favoriteRecipes.find(ingredient => {
+  //     return ingredient.name.includes(input)
+  //   })
+  //   if (!match) {
+  //     return "Sorry, we could not find any recipes to match your search"
+  //   } else {
+  //     this.recipes.filter(recipe => {
+  //       recipe.ingredients.forEach(ingredient => {
+  //         if (ingredient.id === match.id) {
+  //           matches.push(recipe)
+  //         }
+  //       })
+  //     })
+  //     return matches
+  //   }
+  // }
 
 }
 
